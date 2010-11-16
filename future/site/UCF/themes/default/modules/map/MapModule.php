43d8bc968ca9f12bb62cf0372d418236cddcbf56
Douglas Beck <dbeck@douglasbeck.com>
2010-11-16 13:01:43 -0500

    Initializing map

---
+++ future/site/UCF/themes/default/modules/map/MapModule.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* UCF Mobile Map
+* Web Communications - Fall 2010
+* 
+* @author Douglas Beck
+*/
+
+require_once realpath(LIB_DIR.'/Module.php');
+
+class MapModule extends Module {
+  protected $id = 'map';
+  protected function initializeForPage() {}
+}
