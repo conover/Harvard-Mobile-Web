82e9aa26791714d5a8ec8b3f63836f719647e22e
Jared Lang <jslang@mail.ucf.edu>
2010-12-02 12:44:23 -0500

    Created faq module.  Answers are currently not displayed because we may be able to alter AskUCF to display answers in RSS feeds, but it currently doesnt do this.

--- future/site/UCF/themes/default/modules/faq/FaqModule.php
+++ future/site/UCF/themes/default/modules/faq/FaqModule.php
@@ -10,7 +10,43 @@ class FaqModule extends Module {
 	
 	protected $id = 'faq';
 	
+	function getFeed($url){
+		$dummy = array(
+			'TITLE'            => "None",
+			'SLUG'             => "none",
+			'BASE_URL'         => $url,
+			'CONTROLLER_CLASS' => "GazetteRSSController",
+			'ITEM_CLASS'       => "GazetteRSSItem",
+			'ENCLOSURE_CLASS'  => "GazetteRSSEnclosure",
+			'MEDIAGROUP_CLASS' => "GazetteRSSMediaGroup",
+		);
+		$feed = RSSDataController::factory($dummy);
+		return $feed;
+		
+	}
+	
+	function initialize(){
+		$this->options = $GLOBALS['siteConfig']->getSection($this->id);
+	}
+	
+	function search($q){
+		$url     = $this->options['FAQ_URL'];
+		$qstring = str_replace('%q', urlencode($q), $this->options['FAQ_QUERY']);
+		$feed    = $this->getFeed($url.'?'.$qstring);
+		return $feed->items();
+	}
+	
 	function initializeForPage(){
+		$q = $this->getArg('q', '');
+		if ($q != ''){
+			$items = $this->search($q);
+			$this->assign('items', $items);
+			$this->assign('q', $q);
+		}else{
+			$this->assign('items', array());
+			$this->assign('q', null);
+		}
+		
 		
 	}
 }
--- future/site/UCF/themes/default/modules/faq/index.tpl
+++ future/site/UCF/themes/default/modules/faq/index.tpl
@@ -1,10 +1,27 @@
 {extends file="findExtends:common/base.tpl"}
 
 {block name="body"}
-<div id="text">
-	<h2>Coming soon</h2>
-	<div class="block">
-		<p>Module not yet implemented</p>
-	</div>
+<div>
+	<h2>UCF Help</h2>
+	<form action="" method="get"><div>
+		<input type="search" placeholder="Ask a question" name="q" />
+		<input type="submit" value="Search" />
+	</div></form>
+	{if $items}
+	<ul id="text">
+	{foreach $items as $item}
+		<li class="block">
+			<h3 class="question">{$item->getTitle()}</h3>
+			<div class="answer">
+				I am a robot
+			</div>
+		</li>
+	{/foreach}
+	</ol>
+	{else if $q}
+	<p>No help found for '{$q}'.</p>
+	{else}
+		
+	{/if}
 </div>
 {/block}
