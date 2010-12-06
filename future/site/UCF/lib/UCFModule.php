202cdc1a2c15c24307196760a08a8f844b49bda8
Jared Lang <jslang@mail.ucf.edu>
2010-12-06 13:52:13 -0500

    Added caching methods to module, allowing http urls to be pulled and cached easily.

--- future/site/UCF/lib/UCFModule.php
+++ future/site/UCF/lib/UCFModule.php
@@ -17,6 +17,79 @@ abstract class UCFModule extends Module{
 		$data = file_get_contents($url, false, $options);
 		return $data;
 	}
+	
+	/**
+	 * Return the contents of a given url using framework's caching system.
+	 *
+	 * @return void
+	 * @author Jared Lang
+	 **/
+	function fromCache($url)
+	{
+		$contents = $this->getCache($url);
+		return $contents;
+	}
+	
+	
+	/**
+	 * Define where this module's cache belongs.
+	 *
+	 * @return void
+	 * @author Jared Lang
+	 **/
+	function cacheFolder()
+	{
+		return CACHE_DIR.'/'.urlencode($this->id);
+	}
+	
+	
+	/**
+	 * Return a unique key for the given url.
+	 *
+	 * @return void
+	 * @author Jared Lang
+	 **/
+	function cacheKey($url)
+	{
+		$key = md5($url);
+		return $key;
+	}
+	
+	
+	/**
+	 * Write content to cache defined by key
+	 *
+	 * @return void
+	 * @author Jared Lang
+	 **/
+	function setCache($key, $content)
+	{
+		$file     = $key;
+		$lifespan = $GLOBALS['siteConfig']->getVar('DEFAULT_CACHE_LIFESPAN');
+		$cache    = new DiskCache($this->cacheFolder(), $lifespan, TRUE);
+		$cache->write($content, $file);
+	}
+	
+	
+	/**
+	 * Return contents of url, creating new version of cache if out of date or
+	 * non-existent
+	 *
+	 * @return void
+	 * @author Jared Lang
+	 **/
+	function getCache($url)
+	{
+		$key      = $this->cacheKey($url);
+		$lifespan = $GLOBALS['siteConfig']->getVar('DEFAULT_CACHE_LIFESPAN');
+		$cache    = new DiskCache($this->cacheFolder(), $lifespan, TRUE);
+		
+		if (!$cache->isFresh($key)){
+			$data = $this->fetchHttp($url);
+			$cache->write($data, $key);
+		}
+		return $cache->read($key);
+	}
 }
 
 ?>
