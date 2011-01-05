9d73735bd7dda002d01c55ebad4b0e16466479fd
Jim Kang <jim.kang@modolabs.com>
2011-01-05 18:36:09 -0500

    [#413] Added 'federated' flag to url parameter. So, 'more results' links on the federated search results page will have a federated=1 in the query string.

--- future/lib/Module.php
+++ future/lib/Module.php
@@ -775,9 +775,10 @@ abstract class Module {
     return 0;
   }
   
-  protected function urlForSearch($searchTerms) {
+  protected function urlForFederatedSearch($searchTerms) {
     return $this->buildBreadcrumbURL("/{$this->id}/search", array(
       'filter' => $searchTerms,
+      'federatedSource' => 1
     ), false);
   }
 }
